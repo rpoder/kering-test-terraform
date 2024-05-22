593070426d12d021914e997dd6d91319baf5a2d5
rpoder <rpoder@student.42.fr>
2024-05-22 10:44:44 +0200

    FIX: target group container

--- apps/app-3/lb.tf
+++ apps/app-3/lb.tf
@@ -1,10 +1,3 @@
-resource "aws_lb_target_group" "tg" {
-  name     = "${var.project_name}-TG"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-}
-
 resource "aws_lb" "lb" {
   name               = "${var.project_name}-LB"
   internal           = false
@@ -12,10 +5,32 @@ resource "aws_lb" "lb" {
   security_groups    = [aws_security_group.sg.id]
   subnets            = var.public_subnets
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     Name       = "${var.project_name}-LB"
     ProjetName = var.project_name
   }
 }
+
+resource "aws_lb_target_group" "tg" {
+  name        = "${var.project_name}-TG"
+  target_type = "ip"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.lb.arn
+
+  port     = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+
--- apps/app-3/service.tf
+++ apps/app-3/service.tf
@@ -6,14 +6,14 @@ resource "aws_ecs_service" "service" {
   name            = "${var.project_name}-SVC"
   cluster         = data.aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.td.arn
+  desired_count   = 1
   network_configuration {
     subnets         = var.private_subnets
     security_groups = [aws_security_group.sg.id]
   }
-  desired_count = 1
   load_balancer {
-    target_group_arn = aws_ecs_task_definition.td.arn
-    container_name   = "${var.project_name}-CONTAINER"
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "my-nginx"
     container_port   = 80
   }
 }
